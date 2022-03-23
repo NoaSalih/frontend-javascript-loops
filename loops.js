ef6a249be864af9e4ea0e9d8af862b7498b762d6
NoaSalih <nawar2000@hotmail.com>
2022-03-23 22:16:45 +0100

    first commit

--- loops.js
+++ loops.js
@@ -5,7 +5,9 @@
 // Hoera!
 // Hoera!
 // ==========================================
-
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!")
+}
 
 
 // ==========================================
@@ -16,38 +18,38 @@
 // loop...
 // loop...
 // klaar!
- // ==========================================
-
+// ==========================================
+for (let i = 0; i < 5; i++) {
+    if (i < 4) {
+        console.log("loop...")
+    } else (console.log("klaar!"))
+}
 
 
 // ==========================================
-// Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
-// Verwachte uitkomsten:
-// 0 is even
-// 1 is oneven
-// 2 is even
-// 3 is oneven
-// .... etc.
-// 15 is oneven
+// Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even
+// of oneven is. Verwachte uitkomsten: 0 is even 1 is oneven 2 is even 3 is oneven .... etc. 15 is oneven
 
-// Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
+// Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door
+// 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
-
-
+for (let i = 1; i <= 15; i++) {
+    if (i % 2) {
+        console.log(i + " is oneven")
+    } else (console.log(i + " is even"))
+}
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
-// Echter, vanaf het getal 3 komt er een > voor ieder getal te staan, en vanaf het getal 6 komt er >> voor ieder getal te staan.
-// Verwachte uitkomsten:
-// 0
-// 1
-// 2
-// > 3
-// > 4
-// > 5
-// >> 6
-// >> 7
-// >> 8
-// >> 9
-// ==========================================
+// Echter, vanaf het getal 3 komt er een > voor ieder getal te staan, en vanaf het getal 6 komt er >> voor ieder getal
+// te staan. Verwachte uitkomsten: 0 1 2 > 3 > 4 > 5 >> 6 >> 7 >> 8 >> 9 ==========================================
+let sign = "";
+for (let i = 0; i < 10; i++) {
+    if (i >= 3 && i < 6) {
+        sign = "> ";
+    } else if (i >= 6) {
+        sign = ">> ";
+    }
+    console.log(sign + i);
 
+}
