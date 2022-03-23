c006157f6710e4e004a0562fad1a51038b8f35bc
NoaSalih <nawar2000@hotmail.com>
2022-03-23 23:07:30 +0100

    alles af

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -1,18 +1,24 @@
 // ==========================================
 // Opdracht 1
-// Schrijf een script dat iedere naam in onderstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
-// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
+// Schrijf een script dat iedere naam in onderstaande array vervangt door een koosnaampje, door er -"je" achter te
+// plakken. Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
 // ==========================================
 
-const names = ["Henk", "Piet", "Fred", "Joop"];
+const names = ["Henk", "Piet", "Fred", "Joop", "Sint"];
+const je = "je";
 
+console.log(names);
+
+for (let i = 0; i < names.length; i++) {
+    names[i] = names[i] + je;
+}
+
+console.log(names);
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
-
-
 
 
 // ==========================================
@@ -28,8 +34,16 @@ const numbers = [2, 4, 5, 29, 38];
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
+console.log(numbers);
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2) {
+        numbers[i] = numbers[i] * 3;
 
-
+    } else {
+        numbers[i] = numbers[i] * 2;
+    }
+}
+console.log(numbers);
 
 
 // ==========================================
@@ -41,6 +55,11 @@ const numbers = [2, 4, 5, 29, 38];
 // ==========================================
 
 const squares = [30, 2, 8, 24, 11];
+console.log(squares);
+for (let i = 0; i < squares.length; i++) {
+    console.log("Het volume van " +squares[i]+ " is " + squares[i]*squares[i]*squares[i]);
+}
+
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -52,4 +71,4 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 8 is 512',
 //   'Het volume van 24 is 13824',
 //   'Het volume van 11 is 1331'
-// ]
+//
